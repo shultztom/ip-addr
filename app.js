6d6f3bb3dd847c735afb37c3972c38cc0a17100b
shultztom <shultzthomask@sau.edu>
2020-02-15 12:39:17 -0600

    change to ip getter and added cloudbuild

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
+// app.set('trust proxy',true); 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const {ip} = req;
+  const ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
   res.status(200).json({ip})
 });
 
