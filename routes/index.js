745b8fb9ad772465a0d6e676be04ea2986cc9676
shultztom <shultzthomask@sau.edu>
2020-02-15 13:23:23 -0600

    worked on cloudbuild and getting ip address

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const ip = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  let ip = req.headers['x-forwarded-for'] || 
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+  try {
+    ip = ip.split(',')[0]
+  } catch (error) {
+    console.log('ERROR: Issue splitting IP Address')
+    ip = null;
+  }
+    
   res.status(200).json({ip})
 });
 
