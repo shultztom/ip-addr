1354144dd8ec498177e4c0554b6569efd9d8f54e
shultztom <shultzthomask@sau.edu>
2020-02-23 11:33:44 -0600

    added MaxMind db for more info

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,84 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const maxmind = require("maxmind");
 
 // Return as JSON
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   const ip = getIP(req);
-  res.status(200).json({ip})
+  const maxMindData = getMaxMindData(ip);
+  if (maxMindData) {
+    maxMindData
+      .then(geoData => {
+        if (geoData) {
+          let ipInfo = {
+            city: geoData.city.names.en,
+            state: geoData.subdivisions[0].iso_code,
+            postalCode: geoData.postal.code,
+            country: geoData.country.names.en,
+            latitude: geoData.location.latitude,
+            longitude: geoData.location.longitude,
+            timezone: geoData.location.time_zone,
+            msg:
+              "Data provided by MaxMind GeoLite2 Databases, you can find out more at https://maxmind.com"
+          };
+          ipInfo = { ip, ...ipInfo };
+          return res.status(200).json(ipInfo);
+        } else {
+          // No geoData, just send ip address
+          return res.status(200).json({ ip });
+        }
+      })
+      .catch(err => {
+        // Error getting geoData, just send ip address
+        console.log(err);
+        return res.status(200).json({ ip });
+      });
+  } else {
+    // No geoData, just send ip address
+    return res.status(200).json({ ip });
+  }
 });
 
-const getIP = (req) => {
-  let ip = req.headers['x-forwarded-for'] || 
-    req.connection.remoteAddress || 
+const getIP = req => {
+  let ip =
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     (req.connection.socket ? req.connection.socket.remoteAddress : null);
 
   try {
-    ip = ip.split(',')[0]
+    ip = ip.split(",")[0];
   } catch (error) {
-    console.log('ERROR: Issue splitting IP Address')
+    console.log("ERROR: Issue splitting IP Address");
     ip = null;
   }
   try {
-    if(ip.includes('::ffff:')){
-      ip = ip.replace('::ffff:', '');
+    if (ip.includes("::ffff:")) {
+      ip = ip.replace("::ffff:", "");
     }
   } catch (error) {
-    console.log('ERROR: Issue cleaning IP Address')
+    console.log("ERROR: Issue cleaning IP Address");
     ip = null;
   }
 
   return ip;
-}
+};
+
+const getMaxMindData = ip => {
+  if (ip) {
+    return maxmind
+      .open("dbs/GeoLite2-City.mmdb")
+      .then(lookup => {
+        return lookup.get(ip);
+      })
+      .catch(err => {
+        console.log("Error with City Lookup");
+        console.log(err);
+        return null;
+      });
+  } else {
+    return null;
+  }
+};
 
 module.exports = router;
