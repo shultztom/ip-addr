9694c260e08c52a7e136e96e40544450b8a3f2cf
shultztom <shultzthomask@sau.edu>
2020-08-02 10:19:50 -0500

    got latest and cleaned up

--- nexus-docker-deploy.sh
+++
@@ -1,4 +0,0 @@
-docker build -t shultztom/ip-addr .
-docker tag shultztom/ip-addr:latest nexus-docker-internal.shultzlab.com/shultztom/ip-addr:latest
-docker push nexus-docker-internal.shultzlab.com/shultztom/ip-addr:latest
-echo 'Done pushing image'
